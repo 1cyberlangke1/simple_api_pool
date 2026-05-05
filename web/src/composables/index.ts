3428154d33df7840feedc55419321a3c2f262181
赛博浪客 <13411627+cyberlangke@user.noreply.gitee.com>
2026-05-05 20:01:33 +0800

    chore: remove all legacy TypeScript/Node.js codebase

--- web/src/composables/index.ts
+++
@@ -1,8 +0,0 @@
-export { useQuota } from "./useQuota";
-export { useCurrency } from "./useCurrency";
-export { useKey } from "./useKey";
-export { usePolling, formatBytes, formatHitRate, formatNumber } from "./usePolling";
-export { useFormatters, formatHour } from "./useFormatters";
-export { useSSE, useDashboardSSE, type SSEEventMap, type SSEConnectionState } from "./useSSE";
-export { useDashboardData, type StatsData, type HealthData, type UseDashboardDataReturn } from "./useDashboardData";
-export { useUptime, formatUptime, type UseUptimeReturn } from "./useUptime";
